6a1c3331d7e2aca2e9cd97baccc6f613a9468b88
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-11-25 13:53:12 +0100

    Basic form functionality

--- src/app/components/define-criteria/define-criteria.component.ts
+++ src/app/components/define-criteria/define-criteria.component.ts
@@ -1,15 +1,36 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+interface ICriteria {
+  title: any;
+  minmax: any;
+}
 
 @Component({
   selector: 'app-define-criteria',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './define-criteria.component.html',
   styleUrl: './define-criteria.component.css',
 })
 export class DefineCriteriaComponent {
-  criteriaArray = [];
+  criteriaArray: any[] = [];
 
-  ngOnInit() {}
+  formGroup = new FormGroup({
+    title: new FormControl('', [Validators.minLength(3)]),
+    minmax: new FormControl('MIN', [Validators.required]),
+  });
+
+  addCriteria() {
+    if (this.formGroup.valid) {
+      this.criteriaArray.push(this.formGroup.value);
+      this.formGroup.reset();
+    }
+  }
 }
