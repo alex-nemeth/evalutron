817163bb3f2eb94039037dd346c1d4c4ba50a700
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-11-27 16:45:07 +0100

    Base restyling, welcome component, routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { DefineCriteriaComponent } from './components/define-criteria/define-criteria.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'MCEA',
+    component: WelcomeComponent,
+  },
+  {
+    path: 'criteria',
+    title: 'Define Criteria',
+    component: DefineCriteriaComponent,
+  },
+];
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
+    colors: {
+      "mcea-1": "#164863",
+      "mcea-2": "#427D9D",
+      "mcea-3": "#9BBEC8",
+      "mcea-4": "#DDF2FD",
+      white: colors.white,
+      black: colors.black,
+    },
     extend: {},
   },
   plugins: [],
