353434530441f7cb1bd6ab0f4b4842d1abda4175
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2023-12-12 23:13:37 +0100

    Alternative definition draft

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { DefineCriteriaComponent } from './components/define-criteria/define-criteria.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { EstimationOfWeightsComponent } from './components/estimation-of-weights/estimation-of-weights.component';
+import { DefineAlternativesComponent } from './components/define-alternatives/define-alternatives.component';
 
 export const routes: Routes = [
   {
@@ -18,5 +19,10 @@ export const routes: Routes = [
     path: 'weights-estimation',
     title: 'Estimation of Weights',
     component: EstimationOfWeightsComponent,
+  },
+  {
+    path: 'alternatives',
+    title: 'Define Alternatives',
+    component: DefineAlternativesComponent,
   },
 ];
---
+++ src/app/components/define-alternatives/define-alternatives.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ICriteria } from '../../models/criteria.model';
+import { CriteriaService } from '../../services/criteria.service';
+import { of } from 'rxjs';
+import { ButtonComponent } from '../button/button.component';
+
+@Component({
+  standalone: true,
+  selector: 'app-define-alternatives',
+  templateUrl: './define-alternatives.component.html',
+  imports: [CommonModule, ReactiveFormsModule, RouterModule, ButtonComponent],
+})
+export class DefineAlternativesComponent {
+  criteria = this.criteriaService.criteria;
+
+  formGroup = new FormGroup({
+    id: new FormControl(''),
+    title: new FormControl('', [Validators.required]),
+    minmax: new FormControl('MIN', [Validators.required]),
+  });
+
+  constructor(private criteriaService: CriteriaService) {}
+
+  addCriteria() {
+    if (this.formGroup.valid) {
+      this.criteriaService.addCriteria(this.formGroup.value as ICriteria);
+      this.formGroup.reset();
+      this.formGroup.controls.minmax.setValue('MIN');
+    }
+  }
+}
