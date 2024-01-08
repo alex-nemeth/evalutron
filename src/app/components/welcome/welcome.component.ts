585d090951d42026fce80b4a778516c62bd73c8b
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2024-01-08 18:16:00 +0100

    Welcome component demo button

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -2,6 +2,8 @@ import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 import { NavButtonComponent } from "../common/nav-button/button.component";
+import { AlternativeService } from "../../services/alternative.service";
+import { CriteriaService } from "../../services/criteria.service";
 
 @Component({
     standalone: true,
@@ -9,4 +11,20 @@ import { NavButtonComponent } from "../common/nav-button/button.component";
     templateUrl: "./welcome.component.html",
     imports: [CommonModule, RouterModule, NavButtonComponent],
 })
-export class WelcomeComponent {}
+export class WelcomeComponent {
+
+    constructor(
+        private alternativeService: AlternativeService,
+        private criteriaService: CriteriaService
+    ){}
+
+    ngOnInit() {
+        this.alternativeService.clearData();
+        this.criteriaService.clearData();
+    }
+
+    loadDemoData() {
+        this.alternativeService.loadDemoAlternatives();
+        this.criteriaService.loadDemoCriteria();
+    }
+}
