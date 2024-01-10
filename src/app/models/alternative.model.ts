f0eafa9b6b1acd84d1c115830551fb40c3d92576
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2024-01-10 20:57:38 +0100

    new alternative interface draft

--- src/app/models/alternative.model.ts
+++ src/app/models/alternative.model.ts
@@ -1,3 +1,20 @@
 export interface IAlternative {
     [key: string]: any;
 }
+
+// For future refactoring
+// export interface IAlternative {
+//     id: string;
+//     title: string;
+//     values: {
+//         raw?: {
+//             [key: string]: number;
+//         };
+//         calculated?: {
+//             [key: string]: number;
+//         };
+//         normalized?: {
+//             [key: string]: number;
+//         }
+//     }
+// }
