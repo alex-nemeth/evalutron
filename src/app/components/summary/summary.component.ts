c4efdfcf44d9d9a4eea60cefdcbf5d0180e50958
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2024-01-26 20:34:12 +0100

    #11 Summary and nav buttons

--- src/app/components/summary/summary.component.ts
+++ src/app/components/summary/summary.component.ts
@@ -1,43 +1,47 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NavButtonComponent } from '../common/nav-button/button.component';
-import { AlternativeService } from '../../services/alternative.service';
-import { WeightService } from '../../services/weight.service';
-import { CriteriaService } from '../../services/criteria.service';
-import { IAlternative } from '../../models/alternative.model';
-import { MatTableModule } from '@angular/material/table';
-import { LoadingService } from '../../services/loading.service';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { NavButtonComponent } from "../common/nav-button/button.component";
+import { AlternativeService } from "../../services/alternative.service";
+import { WeightService } from "../../services/weight.service";
+import { CriteriaService } from "../../services/criteria.service";
+import { IAlternative } from "../../models/alternative.model";
+import { MatTableModule } from "@angular/material/table";
+import { LoadingService } from "../../services/loading.service";
+import { TranslateModule } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-summary',
-  standalone: true,
-  imports: [CommonModule, NavButtonComponent, MatTableModule],
-  templateUrl: './summary.component.html'
+    selector: "app-summary",
+    standalone: true,
+    imports: [
+        CommonModule,
+        NavButtonComponent,
+        MatTableModule,
+        TranslateModule,
+    ],
+    templateUrl: "./summary.component.html",
 })
 export class SummaryComponent {
+    alternatives!: IAlternative[];
 
-  alternatives!: IAlternative[];
+    constructor(
+        private alternativeService: AlternativeService,
+        private weightService: WeightService,
+        private criteriaService: CriteriaService,
+        private loadingService: LoadingService
+    ) {}
 
-  constructor(
-    private alternativeService: AlternativeService,
-    private weightService: WeightService,
-    private criteriaService: CriteriaService,
-    private loadingService: LoadingService
-    ) { }
+    ngOnInit() {
+        this.alternatives = this.alternativeService.alternatives;
+        this.alternativeService.calculateWeightedSums();
+    }
 
-  ngOnInit() {
-    this.alternatives = this.alternativeService.alternatives;
-    this.alternativeService.calculateWeightedSums();
-  }
+    ngAfterViewInit(): void {
+        this.loadingService.hide();
+    }
 
-  ngAfterViewInit(): void {
-    this.loadingService.hide();
-  }
-
-  sortBySum(alternatives: IAlternative[]) {
-    return alternatives.sort((a, b) => {
-      return b.values.weightedSum! - a.values.weightedSum!;
-    })
-  }
-
+    sortBySum(alternatives: IAlternative[]) {
+        return alternatives.sort((a, b) => {
+            return b.values.weightedSum! - a.values.weightedSum!;
+        });
+    }
 }
