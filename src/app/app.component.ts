2d68e0fb2cd0ed3dccd143bb259429e2dc487d7f
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2024-01-27 17:27:12 +0100

    #50 Navbar implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from "@angular/common";
 import { RouterOutlet } from "@angular/router";
 import { WelcomeComponent } from "./components/welcome/welcome.component";
 import { DefineCriteriaComponent } from "./components/define-criteria/define-criteria.component";
+import { NavbarComponent } from "./components/common/navbar/navbar.component";
 
 @Component({
     selector: "app-root",
@@ -12,9 +13,10 @@ import { DefineCriteriaComponent } from "./components/define-criteria/define-cri
         RouterOutlet,
         WelcomeComponent,
         DefineCriteriaComponent,
+        NavbarComponent,
     ],
     templateUrl: "./app.component.html",
 })
 export class AppComponent {
-    title = "MCEA";
+    title = "Evalutron";
 }
