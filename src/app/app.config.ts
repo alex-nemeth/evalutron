6aa2db26771f19d94968f499f12ed731f2ca4998
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2024-01-30 23:41:02 +0100

    #48 Config update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,23 +2,27 @@ import { ApplicationConfig } from "@angular/core";
 import { provideRouter } from "@angular/router";
 
 import { routes } from "./app.routes";
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimations } from "@angular/platform-browser/animations";
 import { HttpClient, provideHttpClient } from "@angular/common/http";
 import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
 import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 
 export function HttpLoaderFactory(http: HttpClient) {
-    return new TranslateHttpLoader(http);
+    return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
 }
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes), provideAnimations(), provideHttpClient(),
-    TranslateModule.forRoot({
-        defaultLanguage: 'en',
-        loader: {
-            provide: TranslateLoader,
-            useFactory: HttpLoaderFactory,
-            deps: [HttpClient]
-        }
-    }).providers!],
+    providers: [
+        provideRouter(routes),
+        provideAnimations(),
+        provideHttpClient(),
+        TranslateModule.forRoot({
+            defaultLanguage: "en",
+            loader: {
+                provide: TranslateLoader,
+                useFactory: HttpLoaderFactory,
+                deps: [HttpClient],
+            },
+        }).providers!,
+    ],
 };
