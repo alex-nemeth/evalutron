6492a166c2dc96ce70ea6a6b0211bd986a804d02
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2024-02-03 09:30:22 +0100

    #54 Added support colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,22 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
-  theme: {
-    colors: {
-      "mcea-1": "#164863",
-      "mcea-2": "#427D9D",
-      "mcea-3": "#9BBEC8",
-      "mcea-4": "#DDF2FD",
-      white: colors.white,
-      black: colors.black,
-      dark: '#303030',
-      primary: '#9c27b0',
-      accent: '#69f0ae'
+    content: ["./src/**/*.{html,ts}"],
+    theme: {
+        colors: {
+            "mcea-1": "#164863",
+            "mcea-2": "#427D9D",
+            "mcea-3": "#9BBEC8",
+            "mcea-4": "#DDF2FD",
+            white: colors.white,
+            black: colors.black,
+            dark: "#303030",
+            primary: "#9c27b0",
+            accent: "#69f0ae",
+            error: "#f44336",
+            warning: "#ffe83b",
+        },
+        extend: {},
     },
-    extend: {},
-  },
-  plugins: [],
+    plugins: [],
 };
