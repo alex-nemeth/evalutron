a40eb889bcb361cc159e7995c716da1146386a83
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2024-02-11 19:45:20 +0100

    #33 Cleanup of logic into service

--- src/app/components/common/alternatives-grid/alternatives-grid.component.ts
+++ src/app/components/common/alternatives-grid/alternatives-grid.component.ts
@@ -59,32 +59,12 @@ export class AlternativesGridComponent {
         this.table.renderRows();
     }
 
-    logKey(key: string) {
-        // TODO: Implement this as a tooltip on the info icon hover
-        // &: Do not render the info icon if there is no description
-        //       This will require a function to pass for an ngIf
-        //       inside the mat-icon tag.
-        console.log(
-            this.criteriaService.criteria.find(
-                (criteria) => criteria.title === key
-            )?.description
-        );
-    }
-
-    getTooltip(key: string): string {
-        return (
-            this.criteriaService.criteria.find(
-                (criteria) => criteria.title === key
-            )?.description || ""
-        );
+    getTooltip(criterionTitle: string): string {
+        return this.criteriaService.getCriterionDescription(criterionTitle);
     
     }
 
     hasDescription(key: string): boolean {
-        return (
-            this.criteriaService.criteria.find(
-                (criteria) => criteria.title === key
-            )?.description !== undefined
-        );
+        return this.criteriaService.hasDescription(key);
     }
 }
