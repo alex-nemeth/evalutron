66714bc98ac44c62b3a817159015640824c7e3ae
alex-nemeth <116309918+alex-nemeth@users.noreply.github.com>
2024-03-20 11:36:14 +0100

    #65 Draft

--- src/app/components/simple-weight-estimation/simple-weight-estimation.component.ts
+++ src/app/components/simple-weight-estimation/simple-weight-estimation.component.ts
@@ -1,6 +1,11 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { MatInputModule } from "@angular/material/input";
 import { CriteriaService } from "../../services/criteria.service";
+import { ICriteria } from "../../models/criteria.model";
+import { WeightService } from "../../services/weight.service";
+import { LoadingService } from "../../services/loading.service";
+import { checkWeightInput } from "../../utils/weight.helper";
 import {
     AbstractControl,
     FormControl,
@@ -10,24 +15,21 @@ import {
     ValidationErrors,
     ValidatorFn,
 } from "@angular/forms";
-import { ICriteria } from "../../models/criteria.model";
-import { WeightService } from "../../services/weight.service";
-import { NavButtonComponent } from "../common/nav-button/button.component";
 import { MatTableModule } from "@angular/material/table";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
-import { MatIconModule } from "@angular/material/icon";
-import { LoadingService } from "../../services/loading.service";
 import { TranslateModule } from "@ngx-translate/core";
-import { NavButtonGroupComponent } from "../common/nav-button-group/nav-button-group.component";
-import { checkWeightInput } from "../../utils/weight.helper";
-import { MatTooltipModule } from "@angular/material/tooltip";
-import { SimpleWeightEstimationComponent } from "../simple-weight-estimation/simple-weight-estimation.component";
 
 @Component({
+    selector: "eval-simple-weight-estimation",
     standalone: true,
-    selector: "app-estimation-of-weights",
-    templateUrl: "./estimation-of-weights.component.html",
+    imports: [
+        CommonModule,
+        MatInputModule,
+        MatTableModule,
+        FormsModule,
+        ReactiveFormsModule,
+        TranslateModule,
+    ],
+    templateUrl: "./simple-weight-estimation.component.html",
     styles: `
     input:disabled {
       opacity: .50;
@@ -36,30 +38,13 @@ import { SimpleWeightEstimationComponent } from "../simple-weight-estimation/sim
       margin-left: 0px;
       margin-right:0px;
     }`,
-    imports: [
-        CommonModule,
-        FormsModule,
-        ReactiveFormsModule,
-        NavButtonComponent,
-        NavButtonGroupComponent,
-        SimpleWeightEstimationComponent,
-        MatTableModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatIconModule,
-        MatTooltipModule,
-        TranslateModule,
-    ],
 })
-export class EstimationOfWeightsComponent {
+export class SimpleWeightEstimationComponent implements OnInit {
     criteria!: ICriteria[];
-    weights: any[] = [];
-    sumOfWeights!: number;
-    usingSaatysMethod: boolean = false;
-
     displayedColumns!: string[];
-
+    weights: any[] = [];
     formGroup = new FormGroup({});
+    sumOfWeights!: number;
 
     constructor(
         private criteriaService: CriteriaService,
@@ -84,10 +69,6 @@ export class EstimationOfWeightsComponent {
         this.loadingService.hide();
     }
 
-    isFieldDisabled(id1: string, id2: string) {
-        return id1 === id2;
-    }
-
     updateWeights(e: any) {
         const weight = e.target.value;
         const weightCell: HTMLElement | null = document.querySelector(
@@ -99,24 +80,16 @@ export class EstimationOfWeightsComponent {
                 "red";
             return;
         }
-        const mirrorID = e.target.id.split("-").reverse().join("-");
-        const mirrorControl = this.formGroup.get(mirrorID) as AbstractControl;
-        mirrorControl.setValue(`${this.weightService.mirrorWeight(weight)}`);
-        const mirrorWeightCell: HTMLElement | null = document.querySelector(
-            `#${mirrorID}`
-        );
         // Cringe code below
         // (This targets the parent Material table cell
         // So that the entire cell is highlighted)
         weightCell!.parentElement!.parentElement!.parentElement!.style.backgroundColor =
             "#69f0ae";
-        mirrorWeightCell!.parentElement!.parentElement!.parentElement!.style.backgroundColor =
-            "#69f0ae";
         this.saveWeights();
     }
 
-    getFormControlName(id1: string, id2: string): string {
-        return `${id1}-${id2}`;
+    getFormControlName(id1: string): string {
+        return id1;
     }
 
     saveWeights() {
@@ -130,9 +103,7 @@ export class EstimationOfWeightsComponent {
             sortedWeights.push(arr);
             this.criteria[i].weight = this.weightService.saatysGeomean(arr);
             this.criteria[i].weightPercentage =
-                (this.weightService.saatysGeomean(arr) /
-                    this.weightService.sumOfWeights) *
-                100;
+                (Number(arr[0]) / this.weightService.sumOfWeights) * 100;
         }
         this.weightService.saveWeights(sortedWeights);
         this.sumOfWeights = Number(this.weightService.sumOfWeights.toFixed(3));
@@ -141,19 +112,13 @@ export class EstimationOfWeightsComponent {
     initForm() {
         const formControls: any = {};
         this.criteria.forEach((criteria) => {
-            this.criteria.forEach((criteria2) => {
-                formControls[`${criteria.id}-${criteria2.id}`] =
-                    new FormControl(
-                        {
-                            value: "1",
-                            disabled: this.isFieldDisabled(
-                                criteria.id,
-                                criteria2.id
-                            ),
-                        },
-                        [this.weightValidator()]
-                    );
-            });
+            formControls[`${criteria.id}`] = new FormControl(
+                {
+                    value: "1",
+                    disabled: false,
+                },
+                [this.weightValidator()]
+            );
         });
         this.formGroup = new FormGroup(formControls);
     }
